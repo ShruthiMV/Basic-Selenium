7b06246ce83eceda68dada4295695cfda42b583a
ShruthiMV <66819967+ShruthiMV@users.noreply.github.com>
2026-02-23 11:35:05 +0530

    Update Cric_buzz.java

--- src/test/java/basic/Cric_buzz.java
+++ src/test/java/basic/Cric_buzz.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Cric_buzz {
-
+//recently added
 	public static void main(String[] args) throws InterruptedException {
 WebDriver driver=new ChromeDriver();
 driver.get("https://www.cricbuzz.com/");
